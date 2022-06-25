2afbbd51bbd29bcd6e0d9389c9b9c52175cb8500
Skitzzy <imtiyazmiah@gmail.com>
2022-06-25 22:08:11 +0100

    Removed on chain storage of canvas

--- decanvas/contracts/DeCanvas.sol
+++ decanvas/contracts/DeCanvas.sol
@@ -2,38 +2,42 @@
 
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+//import "hardhat/console.sol";
 
 contract DeCanvas {
     //Struct for storing data about individual pixels
-    struct Pixel {
-        address painter;
-        uint256 colour;
-        uint256 timestamp;
-    }
+    // struct Pixel {
+    //     address painter;
+    //     uint8 colour;
+    // }
+
+    // struct Canvas {
+    //     Pixel[100][100] pixels;
+    // }
 
     //Event for when a pixel is painted
     event Paint(
         address indexed painter,
         uint256 timestamp,
-        Pixel pixel,
-        uint256 xcoord,
-        uint256 ycoord
+        uint256 colour,
+        uint256 pixelID
     );
 
     //Define the canvas
-    Pixel[100][100] canvas;
+    //Canvas canvas;
 
     //Setup owner modifier for function calls
     address owner;
     modifier _ownerOnly() {
         require(msg.sender == owner);
+        _;
     }
 
     //Setup modifier and variable to track when to allow or disallow painting
     bool public allowPaint = true;
     modifier _paintAllowed() {
         require(allowPaint == true);
+        _;
     }
 
     //Runs on initial deploy
@@ -42,45 +46,39 @@ contract DeCanvas {
         owner = msg.sender;
 
         //Initialise each pixel to white, with null address as painter
-        for (uint256 x = 0; x < 100; x++) {
-            for (uint256 y = 0; y < 100; y++) {
-                canvas[x][y].colour = 0;
-                canvas[x][y].painter = address(0);
-                canvas[x][y].timestamp = block.timestamp;
-            }
-        }
-        console.log("DeCanvas contract deployed");
+        // for (uint256 x = 0; x < 100; x++) {
+        //     for (uint256 y = 0; y < 100; y++) {
+        //         canvas.pixels[x][y].colour = 0;
+        //         canvas.pixels[x][y].painter = address(0);
+        //         canvas.pixels[x][y].timestamp = block.timestamp;
+        //     }
+        // }
+        
+        //console.log("DeCanvas contract deployed");
     }
 
     //Paint a pixel, only when allowPaint is true
     function paint(
-        uint256 x,
-        uint256 y,
+        uint256 pixelID,
         uint256 _colour
     ) public _paintAllowed {
         //Update relevant Pixel struct in canvas
-        canvas[x][y].colour = _colour;
-        canvas[x][y].painter = msg.sender;
-        canvas[x][y].timestamp = block.timestamp;
+        //canvas.pixels[x][y].colour = _colour;
+        //canvas.pixels[x][y].painter = msg.sender;
+        //canvas.pixels[x][y].timestamp = block.timestamp;
 
-        console.log(
-            "%s painted pixel at %s , %s with colour %s",
-            msg.sender,
-            x,
-            y,
-            _colour
-        );
-
-        emit Paint(msg.sender, block.timestamp, canvas[x][y], x, y);
+        //console.log("User %s painted pixel at %s , %s with colour %s",msg.sender,x,y,_colour);
+        require(pixelID < 40000);
+        emit Paint(msg.sender, block.timestamp, _colour, pixelID);
     }
 
     //Return the entire canvas
-    function getCanvas() public pure returns (Pixel[][] memory) {
-        return canvas;
-    }
+    // function getCanvas() public view returns (Canvas memory) {
+    //     return canvas;
+    // }
 
     //Allows the owner of the contract to toggle whether painting is allowed
-    function togglePainting() _ownerOnly {
+    function togglePainting() public _ownerOnly {
         allowPaint = !allowPaint;
     }
 }
