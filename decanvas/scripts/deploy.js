6d5585e6530f88b4fb76f0acad441bd99ff981d2
Skitzzy <imtiyazmiah@gmail.com>
2022-06-26 08:08:14 +0100

    Added NFT contract for canvases

--- decanvas/scripts/deploy.js
+++ decanvas/scripts/deploy.js
@@ -6,19 +6,26 @@ const main = async () => {
     console.log("Account balance: ", accountBalance.toString());
 
     const deCanvasContractFactory = await hre.ethers.getContractFactory("DeCanvas");
+    
+    console.log("1");
     const deCanvasContract = await deCanvasContractFactory.deploy();
+    
+    console.log("2");
     await deCanvasContract.deployed();
 
     //testing 
-    let deCanvasTxn = await deCanvasContract.paint(10, 10, 10);
-    await deCanvasTxn.wait();
+    //let deCanvasTxn = await deCanvasContract.paint(1, 1, 1);
+    //await deCanvasTxn.wait();
 
-    let deCanvasTxntwo = await deCanvasContract.paint(15, 12, 25);
-    await deCanvasTxntwo.wait();
+    console.log("3");
+    //let deCanvasTxntwo = await deCanvasContract.paint(5, 2, 5);
+    //await deCanvasTxntwo.wait();
 
-    let deCanvasget;
-    deCanvasget = await deCanvasContract.getCanvas()
-
+    console.log("4");
+    //let deCanvasget;
+    //deCanvasget = await deCanvasContract.getCanvas({gasPrice: 50000000000000000000000000n});
+    
+    console.log("5");
     console.log("deCanvas address: ", deCanvasContract.address);
 };
 
